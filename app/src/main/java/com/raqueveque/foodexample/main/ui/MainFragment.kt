e57da1c2e0b10ed9d096e1a95d21afd308793b77
Roberto <roberto27111989@gmail.com>
2022-03-17 16:54:41 -0300

    Image Zoom

--- app/src/main/java/com/raqueveque/foodexample/main/ui/MainFragment.kt
+++ app/src/main/java/com/raqueveque/foodexample/main/ui/MainFragment.kt
@@ -2,9 +2,11 @@ package com.raqueveque.foodexample.main.ui
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.Matrix
+import android.graphics.PointF
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -39,16 +41,40 @@ import com.raqueveque.foodexample.detail.adapter.VariationsAdapter
 import com.raqueveque.foodexample.detail.constructor.VariationsExtras
 import com.raqueveque.foodexample.main.adapter.FoodAdapter
 import com.raqueveque.foodexample.main.constructor.Food
+import com.squareup.picasso.Picasso
 import java.util.*
 import kotlin.math.abs
 
 
-class MainFragment : Fragment(), IOnBackPressed {
+class MainFragment : Fragment(), IOnBackPressed, View.OnTouchListener {
 
     private var isRevealed = false
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+
+
+    private val TAG = "Touch"
+    private val MIN_ZOOM = 1f
+    val MAX_ZOOM = 1f
+
+    // These matrices will be used to scale points of the image
+    var matrix = Matrix()
+    var savedMatrix = Matrix()
+
+    // The 3 states (events) which the user is trying to perform
+    val NONE = 0
+    val DRAG = 1
+    val ZOOM = 2
+    var mode = NONE
+
+    // these PointF objects are used to record the point(s) the user is touching
+    var start = PointF()
+    var mid = PointF()
+    var oldDist = 1f
+
+    var h = 0
+    var w = 0
 
     //Instancia a la base de datos
     private lateinit var db: FirebaseFirestore
@@ -104,6 +130,8 @@ class MainFragment : Fragment(), IOnBackPressed {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+//        binding.backImage.setOnClickListener { binding.foodImageLayout.visibility = View.GONE }
 
         //Inicializamos las decoraciones del recycler aca, sino se ejecutan una y otra vez
         val dividerItemDecoration = DividerItemDecoration(binding.recyclerMain.context, LinearLayoutManager.VERTICAL)
@@ -251,7 +279,35 @@ class MainFragment : Fragment(), IOnBackPressed {
             }
         })
 
+        binding.foodImage.setOnClickListener(object : DoubleClickListener(){
+            override fun onDoubleClick(v: View?) {
+                Toast.makeText(context, "si", Toast.LENGTH_SHORT).show()
+                binding.foodImage.layoutParams.height = h
+                binding.foodImage.layoutParams.width = w
+            }
+        })
+
         return binding.root
+    }
+
+    // This class has methods that check if two clicks were registered
+    // within a span of DOUBLE_CLICK_TIME_DELTA i.e., in our case
+    // equivalent to 300 ms
+    abstract class DoubleClickListener : View.OnClickListener {
+        var lastClickTime: Long = 0
+        override fun onClick(v: View?) {
+            val clickTime = System.currentTimeMillis()
+            if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA) {
+                onDoubleClick(v)
+            }
+            lastClickTime = clickTime
+        }
+
+        abstract fun onDoubleClick(v: View?)
+
+        companion object {
+            private const val DOUBLE_CLICK_TIME_DELTA: Long = 300 //milliseconds
+        }
     }
 
     //--------------------------------------LAYOUT PRINCIPAL-------------------------------------//
@@ -574,10 +630,11 @@ class MainFragment : Fragment(), IOnBackPressed {
 
         //Se cancela la funcion de atras a menos se cumpla la condicion
         override fun onBackPressed(): Boolean {
-            return if (!binding.detailLayout.isVisible){
+            return if (!binding.detailLayout.isVisible || !binding.foodImageLayout.isVisible){
                 true
             } else {
-                revealLayoutFun()
+                if (binding.detailLayout.isVisible && !binding.foodImageLayout.isVisible) revealLayoutFun()
+                else binding.foodImageLayout.visibility = View.GONE
                 false
             }
         }
@@ -678,6 +735,18 @@ class MainFragment : Fragment(), IOnBackPressed {
             //Paramos el shimmer
             binding.imageShimmer.stopShimmer()
             binding.imageShimmer.visibility = View.GONE
+
+            iAdapter.setOnItemClickListener(object : SliderAdapter.OnItemClickListener{
+                @SuppressLint("ClickableViewAccessibility")
+                override fun onItemClick(position: Int) {
+                    binding.foodImageLayout.visibility = View.VISIBLE
+                    Picasso.get().load(imagesList[position].image).into(binding.foodImage)
+                    h = binding.foodImage.height
+                    w = binding.foodImage.width
+                    binding.foodImage.setOnTouchListener(this@MainFragment)
+                }
+
+            })
         }
 
         //Obtenemos los extras
@@ -881,4 +950,129 @@ class MainFragment : Fragment(), IOnBackPressed {
         }
     }
     //---------------------------------------LAYOUT CARRITO--------------------------------------//
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        val view = v as ImageView
+        view.scaleType = ImageView.ScaleType.MATRIX
+        val scale: Float
+        dumpEvent(event)
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                matrix.set(view.imageMatrix)
+                savedMatrix.set(matrix)
+                start.set(event.x, event.y)
+                Log.d(TAG, "mode=DRAG") // write to LogCat
+                mode = DRAG
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> {
+                mode = NONE
+                Log.d(TAG, "mode=NONE")
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                oldDist = spacing(event)
+                Log.d(TAG, "oldDist=$oldDist")
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix)
+                    midPoint(mid, event)
+                    mode = ZOOM
+                    Log.d(TAG, "mode=ZOOM")
+                }
+            }
+            MotionEvent.ACTION_MOVE -> if (mode == DRAG) {
+                matrix.set(savedMatrix)
+                matrix.postTranslate(
+                    event.x - start.x,
+                    event.y - start.y
+                ) // create the transformation in the matrix  of points
+            } else if (mode == ZOOM) {
+                // pinch zooming
+                val newDist = spacing(event)
+                Log.d(TAG, "newDist=$newDist")
+                if (newDist > 5f) {
+                    matrix.set(savedMatrix)
+                    scale = newDist / oldDist // setting the scaling of the
+                    // matrix...if scale > 1 means
+                    // zoom in...if scale < 1 means
+                    // zoom out
+                    matrix.postScale(scale, scale, mid.x, mid.y)
+                }
+            }
+        }
+        view.imageMatrix = matrix // display the transformation on screen
+        return true // indicate event was handled
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: spacing Parameters: MotionEvent Returns: float Description:
+     * checks the spacing between the two fingers on touch
+     * ----------------------------------------------------
+     */
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: spacing Parameters: MotionEvent Returns: float Description:
+     * checks the spacing between the two fingers on touch
+     * ----------------------------------------------------
+     */
+    private fun spacing(event: MotionEvent): Float {
+        val x = event.getX(0) - event.getX(1)
+        val y = event.getY(0) - event.getY(1)
+        return Math.sqrt((x * x + y * y).toDouble()).toFloat()
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
+     * Description: calculates the midpoint between the two fingers
+     * ------------------------------------------------------------
+     */
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
+     * Description: calculates the midpoint between the two fingers
+     * ------------------------------------------------------------
+     */
+    private fun midPoint(point: PointF, event: MotionEvent) {
+        val x = event.getX(0) + event.getX(1)
+        val y = event.getY(0) + event.getY(1)
+        point[x / 2] = y / 2
+    }
+
+    /** Show an event in the LogCat view, for debugging  */
+    private fun dumpEvent(event: MotionEvent) {
+        val names = arrayOf(
+            "DOWN",
+            "UP",
+            "MOVE",
+            "CANCEL",
+            "OUTSIDE",
+            "POINTER_DOWN",
+            "POINTER_UP",
+            "7?",
+            "8?",
+            "9?"
+        )
+        val sb = StringBuilder()
+        val action = event.action
+        val actionCode = action and MotionEvent.ACTION_MASK
+        sb.append("event ACTION_").append(names[actionCode])
+        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP) {
+            sb.append("(pid ").append(action shr MotionEvent.ACTION_POINTER_ID_SHIFT)
+            sb.append(")")
+        }
+        sb.append("[")
+        for (i in 0 until event.pointerCount) {
+            sb.append("#").append(i)
+            sb.append("(pid ").append(event.getPointerId(i))
+            sb.append(")=").append(event.getX(i).toInt())
+            sb.append(",").append(event.getY(i).toInt())
+            if (i + 1 < event.pointerCount) sb.append(";")
+        }
+        sb.append("]")
+        Log.d("Touch Events ---------", sb.toString())
+    }
 }
